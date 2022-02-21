6251ffdb9924feac33577095f8756826904257f1
Artin <alex_91_100@live.se>
2022-02-21 14:15:57 +0100

    Package Manager Console use

--- Garage3.Data/Migrations/20220221124710_init.cs
+++ Garage3.Data/Migrations/20220221124710_init.cs
@@ -4,10 +4,24 @@
 
 namespace Garage3.Data.Migrations
 {
-    public partial class afterMerge : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonalNo = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "VehicleType",
                 columns: table => new
@@ -21,26 +35,6 @@ namespace Garage3.Data.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_VehicleType", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Member",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    PersonalNo = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
-                    Age = table.Column<int>(type: "int", nullable: false),
-                    VehicleTypeId = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Member", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Member_VehicleType_VehicleTypeId",
-                        column: x => x.VehicleTypeId,
-                        principalTable: "VehicleType",
-                        principalColumn: "Id");
                 });
 
             migrationBuilder.CreateTable(
@@ -112,11 +106,6 @@ namespace Garage3.Data.Migrations
                         principalTable: "Vehicle",
                         principalColumns: new[] { "MemberId", "VehicleTypeId" });
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Member_VehicleTypeId",
-                table: "Member",
-                column: "VehicleTypeId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_ParkingLot_VehicleMemberId_VehicleTypeId",
